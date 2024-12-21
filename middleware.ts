fbb1f6d5c8b5c3dddc0538b5d59d9811e2bf3943
AveJaPl <filip.piatek2005@gmail.com>
2024-12-22 00:36:11 +0100

    middleware with redirections created

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Lista publicznych ścieżek
+const unsignedPaths = ["/login", "/register"];
+const publicPaths = ["/", ...unsignedPaths]
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  console.log("middleware", { token, pathname });
+
+  // Jeśli użytkownik ma token i wchodzi na stronę niezalogowanych → redirect do /dashboard
+  if (token && unsignedPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  // Jeśli użytkownik nie ma tokena i wchodzi na stronę chronioną → redirect do /login
+  if (!token && !publicPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // W innych przypadkach kontynuujemy
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/login", "/register", "/dashboard"], // Obsługiwane ścieżki
+};
