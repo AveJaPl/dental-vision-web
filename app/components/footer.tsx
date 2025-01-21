916035d762b53e8b89f306b8a0abdfc1183a9e0b
AveJaPl <filip.piatek2005@gmail.com>
2025-01-21 01:19:42 +0100

    Poprawa wymiarów menu dolnego aby nie chowało sie na IOS

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -21,7 +21,7 @@ export function Footer() {
   ];
 
   return (
-    <footer className="sticky bottom-0 bg-background border-t shadow-md flex justify-around items-center py-3">
+    <footer className="sticky bottom-0 bg-background border-t shadow-md flex justify-around items-center pb-5 pt-3">
       {navItems.map(({ href, key, Icon }) => (
         <Link
           key={key}
