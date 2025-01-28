f67d981404a9bac28f1c12024cd63e193fe9966d
AveJaPl <filip.piatek2005@gmail.com>
2025-01-28 12:59:05 +0100

    headers added for test

--- lib/sender.ts
+++ lib/sender.ts
@@ -5,6 +5,9 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 interface LoginData {
